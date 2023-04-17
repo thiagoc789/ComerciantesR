5af663ebd51b2c32ed1e1f2678c2bdac2e02a870
thiagoc789 <santiago.cruz@correounivalle.edu.co>
2023-04-17 10:55:41 -0500

    cambio en categorias

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -22,26 +22,27 @@ import { mapTo, delay } from 'rxjs/operators';
 export class HomeComponent implements OnInit {
 
   categorias = [
-    { nombre: 'Restaurantes y comidas rápidas', icono: 'fas fa-hamburger', color: '#FF5733' },
-    { nombre: 'Panaderías y heladerías', icono: 'fas fa-birthday-cake', color: '#FFC300' },
-    { nombre: 'Tiendas y supermercados', icono: 'fas fa-shopping-cart', color: '#DAF7A6' },
-    { nombre: 'Medicamentos y aseo', icono: 'fas fa-pills', color: '#581845' },
-    { nombre: 'Hospedaje y hoteles', icono: 'fa-solid fa-hotel', color: '#F5B7B1' },
-    { nombre: 'Tecnología', icono: 'fa-solid fa-microchip', color: '#FF5733' },
-    { nombre: 'Reparaciones y mantenimientos', icono: 'fas fa-wrench', color: '#7FDBFF' },
-    { nombre: 'Salud y belleza', icono: 'fas fa-spa', color: '#FFDC00' },
-    { nombre: 'Educación', icono: 'fas fa-graduation-cap', color: '#E6E6FA' },
-    { nombre: 'Variedades, detalles y accesorios', icono: 'fas fa-gift', color: '#FFE4E1' },
     { nombre: 'Agropecuarias y mascotas', icono: 'fas fa-paw', color: '#1ABC9C' },
+    { nombre: 'Educación', icono: 'fas fa-graduation-cap', color: '#E6E6FA' },
+    { nombre: 'Ferreterias y electricidad', icono: 'fas fa-screwdriver-wrench', color: '#1ABC9C' },
+    { nombre: 'Hospedaje y hoteles', icono: 'fa-solid fa-hotel', color: '#F5B7B1' },
+    { nombre: 'Manualidades', icono: 'fas fa-palette', color: '#DAF7A6' },
+    { nombre: 'Medicamentos y aseo', icono: 'fas fa-pills', color: '#581845' },
+    { nombre: 'Organización de eventos ', icono: 'fas fa-champagne-glasses', color: '#FFC300' },
     { nombre: 'Papelerias y misceláneas', icono: 'fas fa-pencil-ruler', color: '#F5B7B1' },
     { nombre: 'Recreación', icono: 'fa-regular fa-face-laugh-beam', color: '#6C3483' },
-    { nombre: 'Servicios empresariales', icono: 'fas fa-briefcase', color: '#FF5733' },
-    { nombre: 'Organización de eventos ', icono: 'fas fa-champagne-glasses', color: '#FFC300' },
-    { nombre: 'Manualidades', icono: 'fas fa-palette', color: '#DAF7A6' },
+    { nombre: 'Reparaciones y mantenimientos', icono: 'fas fa-wrench', color: '#7FDBFF' },
+    { nombre: 'Restaurantes y comidas rápidas', icono: 'fas fa-hamburger', color: '#FF5733' },
     { nombre: 'Ropa y calzado', icono: 'fas fa-tshirt', color: '#581845' },
-    { nombre: 'Ferreterias y electricidad', icono: 'fas fa-screwdriver-wrench', color: '#1ABC9C' },
+    { nombre: 'Salud y belleza', icono: 'fas fa-spa', color: '#FFDC00' },
+    { nombre: 'Servicios empresariales', icono: 'fas fa-briefcase', color: '#FF5733' },
+    { nombre: 'Tiendas y supermercados', icono: 'fas fa-shopping-cart', color: '#DAF7A6' },
+    { nombre: 'Tecnología', icono: 'fa-solid fa-microchip', color: '#FF5733' },
+    { nombre: 'Variedades, detalles y accesorios', icono: 'fas fa-gift', color: '#FFE4E1' },
+    { nombre: 'Panaderías y heladerías', icono: 'fas fa-birthday-cake', color: '#FFC300' },
     { nombre: 'Repuestos para vehiculos', icono: 'fas fa-car-burst', color: '#7FDBFF' },
   ];
+
 
 
   botonClicado = false;
@@ -134,10 +135,7 @@ export class HomeComponent implements OnInit {
 
 
   buscarEventos(categoria: string) {
-
-    console.log(categoria)
     this.busqueda = categoria;
-    console.log(this.busqueda)
     if (this.busqueda.trim() !== '') {
       const busquedaNormalizada = this.normalizarTexto(this.busqueda);
       const palabrasBusqueda = busquedaNormalizada.split(' ');
