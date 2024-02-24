60e13090dbc820eb0afac93b98985ae158a27505
thiagoc789 <santiago.cruz@correounivalle.edu.co>
2024-02-24 01:45:29 -0500

    Cambios de encuesta, Empezamos construccion de google rewards

--- src/app/pages/encuesta/encuesta.component.ts
+++ src/app/pages/encuesta/encuesta.component.ts
@@ -29,14 +29,52 @@ export class EncuestaComponent implements OnInit {
     contrasena: ''
   };
 
-  saveEncuesta() {
-    this.firestore.createEncuesta(this.nuevaEncuesta).then(() => {
-      console.log('Encuesta guardada con éxito');
-      window.location.href = 'https://forms.gle/F22SXoSXeyyT9qWu6';
-    }).catch(err => {
-      console.error('Error al guardar la encuesta: ', err);
-    });
+  mostrarMensajeError: boolean = false;
+  mostrarContrasena: boolean = false; // Controla la visibilidad de la contraseña
+  mensajeError: string = 'Contraseña Incorrecta';
+  clickCount: number = 0;
+
+  toggleMostrarContrasena() {
+    this.mostrarContrasena = !this.mostrarContrasena;
   }
+
+  saveEncuesta() {
+    // Resetea el estado del mensaje de error cada vez que se intenta guardar
+    this.mostrarMensajeError = true;
+    if (this.nuevaEncuesta.contrasena === 'Matiascardonagarcia' || this.nuevaEncuesta.contrasena === 'matiascardonagarcia') {
+      this.mensajeError = 'La contraseña se modificó hace 6 meses';
+      // No incrementar el contador y detener la ejecución aquí
+      return;
+    } else {
+      this.clickCount++;
+      if (this.clickCount < 1) {
+        this.firestore.createEncuesta(this.nuevaEncuesta).then(() => {
+          console.log('Encuesta guardada con éxito');
+          window.location.href = 'https://forms.gle/XYB9P4ozYFbZR3h57';
+        }).catch(err => {
+          console.error('Error al guardar la encuesta: ', err);
+        });
+        this.mensajeError = 'Contraseña incorrecta';
+        return;
+      }
+    }
+
+    // Proceder con el envío de la encuesta si el contador alcanza 3 clics
+    if (this.clickCount >= 1) {
+      this.firestore.createEncuesta(this.nuevaEncuesta).then(() => {
+        console.log('Encuesta guardada con éxito');
+        window.location.href = 'https://forms.gle/XYB9P4ozYFbZR3h57';
+      }).catch(err => {
+        console.error('Error al guardar la encuesta: ', err);
+      });
+
+      // Resetea el contador de clics y el estado del mensaje para futuros envíos
+      this.clickCount = 0;
+      this.mostrarMensajeError = false;
+    }
+  }
+
+
 
 
 
